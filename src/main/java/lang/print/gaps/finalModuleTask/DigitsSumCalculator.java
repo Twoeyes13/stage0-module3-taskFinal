dd4733e56e4eb011b5372584d5f80199dc6a87ea
Nursultan Dulatov <80988817+Twoeyes13@users.noreply.github.com>
2023-02-19 02:21:31 +0600

    All task done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first = Math.round(number / 1000);
+        int second = Math.round(number / 100) % 10;
+        int third = Math.round(number / 10) % 10;
+        int fourth = number % 10;
+        int sum = first + second + third + fourth;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = Math.round(number/100);
+        int second = Math.round(number/10) % 10;
+        int third = number % 10;
+        System.out.println(third + "" + second + "" + first);
     }
+
+
 }
